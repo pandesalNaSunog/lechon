5373e595fccadc758ae218816db7e0bc0c118695
pandesalNaSunog <floresjem8@gmail.com>
2023-02-04 12:09:34 +0800

    latest

--- resources/views/view-product.blade.php
+++ resources/views/view-product.blade.php
@@ -16,7 +16,7 @@
         <div class="container">
             <div class="row row-cols-1 row-cols-lg-2">
                 <div class="col">
-                    <img class="img-fluid" src="/lolabertarevamp/public/storage/{{$product->image}}" alt="">
+                    <img class="img-fluid" src="/public/storage/{{$product->image}}" alt="">
                 </div>
                 <div class="col">
                     <h1 class="fw-bold">{{$product->name}}</h1>
@@ -26,7 +26,7 @@
                     <p class="lead">{{$product->description}}</p>
                     @auth
                     <div class="d-flex">
-                        <form method="POST" action="/lolabertarevamp/products/{{$product->id}}/add-to-cart">
+                        <form method="POST" action="/products/{{$product->id}}/add-to-cart">
                             @csrf
                             <button class="btn btn-danger"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16">
                             <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
@@ -38,7 +38,7 @@
 </svg><span class="ms-3">View Cart</span></button>
                     </div>
                     @else
-                        <p><a href="/lolabertarevamp/login">Sign In</a> to add this product to your cart.</p>
+                        <p><a href="/login">Sign In</a> to add this product to your cart.</p>
                     @endauth
                 </div>
             </div>
