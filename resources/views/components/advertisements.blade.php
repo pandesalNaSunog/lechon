fcfe9068552a17e78f518c9e169a1d1c39b767c1
pandesalNaSunog <floresjem8@gmail.com>
2023-02-04 11:17:14 +0800

    latest

--- resources/views/components/advertisements.blade.php
+++ resources/views/components/advertisements.blade.php
@@ -4,13 +4,13 @@
             <div class="col">
                 
                 <div class="card shadow">
-                    <img src="/lolabertarevamp/Mon.jpg" alt="" style="height: 800px; width: 100%; object-fit: cover" class="card-img-top img-fluid">
+                    <img src="/Mon.jpg" alt="" style="height: 800px; width: 100%; object-fit: cover" class="card-img-top img-fluid">
                     <div class="card-body text-center">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" style="color: orange" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
                         </svg><span class="ms-3 text-secondary nunito fs-3">Charcoal Roasted Lechon</span>
                         <div class="mt-5">
-                            <a href="/lolabertarevamp/products">
+                            <a href="/products">
                                 <button class="my-btn px-5 py-2">ORDER NOW</button>
                             </a>
                         </div>
@@ -21,13 +21,13 @@
             <div class="col">
                 
                 <div class="card shadow">
-                    <img src="/lolabertarevamp/heaven.jpg" alt="" style="height: 800px; width: 100%; object-fit: cover" class="card-img-top img-fluid">
+                    <img src="/heaven.jpg" alt="" style="height: 800px; width: 100%; object-fit: cover" class="card-img-top img-fluid">
                     <div class="card-body text-center">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" style="color: orange" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
                         </svg><span class="ms-3 text-secondary nunito fs-3">Made to Order</span>
                         <div class="mt-5">
-                            <a href="/lolabertarevamp/products">
+                            <a href="/products">
                                 <button class="my-btn px-5 py-2">ORDER NOW</button>
                             </a>
                         </div>
