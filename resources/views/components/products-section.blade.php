d3975fdfc0ebadcd633d9082a31f373d788a2773
pandesalNaSunog <floresjem8@gmail.com>
2023-02-04 12:08:25 +0800

    latest

--- resources/views/components/products-section.blade.php
+++ resources/views/components/products-section.blade.php
@@ -15,11 +15,11 @@
             @foreach($products as $product)
                 <div class="col">
                     <div class="card shadow">
-                        <img src="/lolabertarevamp/public/storage/{{$product->image}}" style="height: 200px; width: 100%; object-fit: cover" alt="" class="card-img-top img-fluid">
+                        <img src="/public/storage/{{$product->image}}" style="height: 200px; width: 100%; object-fit: cover" alt="" class="card-img-top img-fluid">
                         <div class="card-body">
                             <h3 class="fw-bold text-truncate">{{$product->name}}</h3>
                             <p class="text-truncate">{{$product->description}}</p>
-                            <a href="/lolabertarevamp/products/{{$product->id}}"><button class="btn btn-outline-danger w-100 mt-3">View</button></a>
+                            <a href="/products/{{$product->id}}"><button class="btn btn-outline-danger w-100 mt-3">View</button></a>
                         </div>
                         
                     </div>
