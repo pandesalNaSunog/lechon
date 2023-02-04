b8c4c41d6fb2b6728937fde0937eb2141a7491d1
pandesalNaSunog <floresjem8@gmail.com>
2023-02-04 11:23:29 +0800

    latest

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -1,6 +1,6 @@
 
     <form action="/lolabertarevamp/products" method="GET">
-        <div class="input-group">
+        <div class="input-group ms-3">
             
             <input required type="text" class="form-control" placeholder="Search" name="search">
             <button class="btn btn-warning">Search</button>
