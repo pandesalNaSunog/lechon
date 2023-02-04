d4ced61f80dce082fa29d492e8155ee690d9a01a
pandesalNaSunog <floresjem8@gmail.com>
2023-02-04 11:38:55 +0800

    latest

--- resources/views/components/admin-nav.blade.php
+++ resources/views/components/admin-nav.blade.php
@@ -2,26 +2,26 @@
 <nav class="navbar navbar-expand-lg bg-orange navbar-dark sticky-top">
     <div class="container">
         <div class="navbar-brand">
-            <a href="/lolabertarevamp/admin"><img src="/lolabertarevamp/public/storage/images/logo.png" style="height: 70px; width: auto; object-fit: cover" alt=""></a>
+            <a href="/admin"><img src="/logo.png" style="height: 70px; width: auto; object-fit: cover" alt=""></a>
         </div>
         <button class="navbar-toggler" data-bs-target="#navmenu" data-bs-toggle="collapse"><span class="navbar-toggler-icon"></span></button>
         <div id="navmenu" class="collapse navbar-collapse">
             <ul class="navbar-nav ms-auto">
 
                 <li class="nav-item">
-                    <a href="/lolabertarevamp/admin/users" class="fw-bold nav-link <?php if($active == "users"){ echo 'active'; } ?>">Users</a>
+                    <a href="/admin/users" class="fw-bold nav-link <?php if($active == "users"){ echo 'active'; } ?>">Users</a>
                 </li>
                 <li class="nav-item">
-                    <a href="/lolabertarevamp/admin/inventory" class="fw-bold nav-link <?php if($active == "inventory"){ echo 'active'; } ?>">Inventory</a>
+                    <a href="/admin/inventory" class="fw-bold nav-link <?php if($active == "inventory"){ echo 'active'; } ?>">Inventory</a>
                 </li>
                 <li class="nav-item">
-                    <a href="/lolabertarevamp/admin/orders" class="fw-bold nav-link <?php if($active == "orders"){ echo 'active'; } ?>">Orders</a>
+                    <a href="/admin/orders" class="fw-bold nav-link <?php if($active == "orders"){ echo 'active'; } ?>">Orders</a>
                 </li>
                 <li class="nav-item">
-                    <a href="/lolabertarevamp/admin/sales" class="fw-bold nav-link <?php if($active == "sales"){ echo 'active'; } ?>">Sales Report</a>
+                    <a href="/admin/sales" class="fw-bold nav-link <?php if($active == "sales"){ echo 'active'; } ?>">Sales Report</a>
                 </li>
                 <li class="nav-item">
-                    <a href="/lolabertarevamp/admin/logout" class="fw-bold nav-link">Log Out</a>
+                    <a href="/admin/logout" class="fw-bold nav-link">Log Out</a>
                 </li>
             </ul>
         </div>
